ced8bcf3765f732553b3086abdd04d7349ce6deb
Rodrigo Pereira <rodrigorpogo@gmail.com>
2023-03-09 17:17:17 -0300

    Improving docker compose

--- configs/init.sql
+++ configs/init.sql
@@ -1,8 +1,8 @@
-CREATE USER crud_sql WITH PASSWORD 'crud-password';
+CREATE USER local_admin WITH PASSWORD 'password';
 
-CREATE DATABASE 'crud-sql';
-\c customer;
-CREATE SCHEMA 'crud-sql';
-REVOKE ALL ON DATABASE customer FROM PUBLIC;
-GRANT ALL PRIVILEGES ON DATABASE customer TO customer_user;
-GRANT ALL PRIVILEGES ON SCHEMA customer TO customer_user;
+CREATE DATABASE crud_sql;
+\c crud_sql;
+CREATE SCHEMA crud_sql;
+REVOKE ALL ON DATABASE crud_sql FROM PUBLIC;
+GRANT ALL PRIVILEGES ON DATABASE crud_sql TO local_admin;
+GRANT ALL PRIVILEGES ON SCHEMA crud_sql TO local_admin;
