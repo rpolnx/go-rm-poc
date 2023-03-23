3ce7eb3043676f665492c98e7102b75e52062bf6
Rodrigo Pereira <rodrigorpogo@gmail.com>
2023-03-23 18:00:23 -0300

    seeds

--- configs/migrations/1_create_tables.tx.down.sql
+++ configs/migrations/1_create_tables.tx.down.sql
@@ -1,3 +1,3 @@
+DROP TABLE crud_sql."jobs";
 DROP TABLE crud_sql."users";
 DROP TABLE crud_sql."companies";
-DROP TABLE crud_sql."jobs";
---
+++ configs/migrations/2_seed_tables.tx.down.sql
@@ -0,0 +1,3 @@
+DELETE FROM "crud_sql"."jobs" WHERE true;
+DELETE FROM  "crud_sql"."users" WHERE true;
+DELETE FROM  "crud_sql"."companies" WHERE true;
